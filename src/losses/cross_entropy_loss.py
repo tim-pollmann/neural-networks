8cc88f1b123b4b3e00bd21d4239187039f106a35
tim-pollmann <rollout.sashes.0l@icloud.com>
2023-06-01 16:58:21 +0200

    add cross entropy loss

---
+++ src/losses/cross_entropy_loss.py
@@ -0,0 +1,14 @@
+import numpy as np
+from .loss import Loss
+
+
+class CrossEntropyLoss(Loss):
+    def __init__(self):
+        pass
+
+    def f(self, d, y):
+        return - np.sum(d*np.log(y))
+
+    def f_prime(self, d, y):
+        print(d, y)
+        return -d/y
--- src/train.py
+++ src/train.py
@@ -6,6 +6,7 @@ from layers.fc_layer import FCLayer
 from activation_functions.tanh import Tanh
 from activation_functions.linear import Linear
 from activation_functions.softmax import Softmax
+from losses.cross_entropy_loss import CrossEntropyLoss
 from losses.mse import MSE
 
 
@@ -34,15 +35,15 @@ from losses.mse import MSE
 # # print(out)
 
 x_train = np.array([[[0], [0]], [[0], [1]], [[1], [0]], [[1], [1]]])
-y_train = np.array([[[0], [1]], [[1], [0]], [[1], [0]], [[1], [0]]])
+y_train = np.array([[[0], [1], [0]], [[1], [0], [0]], [[1], [0], [0]], [[1], [0], [0]]])
 
 nn = NeuralNetwork(
     [
         FCLayer(2, 4, Tanh()),
         FCLayer(4, 3, Linear()),
-        FCLayer(3, 2, Softmax())
+        FCLayer(3, 3, Softmax())
     ],
-    MSE()
+    CrossEntropyLoss()
 )
 nn.fit(x_train, y_train, epochs=100, learning_rate=0.1)
 
